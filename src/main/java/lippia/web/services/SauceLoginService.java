e97aa1448ffe4019dd5c96aee46b23fd4c1416fc
Jeny Jensen <jgbony@hotmail.com>
2025-12-15 17:38:30 -0300

    Assert testng

--- src/main/java/lippia/web/services/SauceLoginService.java
+++ src/main/java/lippia/web/services/SauceLoginService.java
@@ -3,6 +3,8 @@ package lippia.web.services;
 import com.crowdar.core.PropertyManager;
 import com.crowdar.core.actions.ActionManager;
 import lippia.web.constants.SauceLoginConstants;
+import org.testng.Assert;
+
 import static com.crowdar.core.actions.WebActionManager.navigateTo;
 
 public class SauceLoginService extends ActionManager {
@@ -33,7 +35,7 @@ public class SauceLoginService extends ActionManager {
     }
 
     public static void verificoMsjPasRequerido() {
-        junit.framework.Assert.assertTrue("No se ve mensaje de error", getText(SauceLoginConstants.ERROR_MSJ_CSS).contains("Epic sadface: Password is required"));
+        Assert.assertTrue(getText(SauceLoginConstants.ERROR_MSJ_CSS).contains("Epic sadface: Password is required"),"No se ve mensaje de password requerido");
 
     }
 }
